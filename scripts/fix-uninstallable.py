43738e6f78347fafa95af99a6227a10b46013770
Martin Cech <emulatorer@gmail.com>
2025-04-07 10:59:33 +0200

    fix the process of adding the next installable revision
    drop unused code, polish readme

--- scripts/fix-uninstallable.py
+++ scripts/fix-uninstallable.py
@@ -1,23 +1,21 @@
-# check all revisions in the lockfile if they are installable
-# remove if not and the next installable version is added
+# Check all revisions in the lockfile if they are installable.
+# Remove if not and add the next installable revision.
 #
-# the script only updates the lock file and does not install
-# or uninstall any tools from a Galaxy instance
+# Only updates the lock file and does not install
+# or uninstall any tools from a Galaxy instance.
 #
-# backgroud for each version there can be only one revision installed
-# (multiple revisions with the same version happen eg if the version
-# is not bumbed)
+# Backgroud: for each tool version there can be only one revision installed
+# (multiple revisions with the same version happen e.g. if the version
+# is not bumped but some files are updated)
 #
-# revisions that became uninstallable can be safely removed since
+# Revisions that became not-installable are treated as a safe update
+# because the author claims the tool did not change its behavior from
+# the reproducibility perspective.
 #
-# - tool revision can not be executed anyway
-# - also if another instance requests to install then the latest
-#   revision with the same version will be installed
-#
-# the script queries the TS to get_ordered_installable_revisions
+# The script queries the TS to get_ordered_installable_revisions
 # and clones (to /tmp/) the mercurial repos to get all revisions
-# (the later is opnly done for tools with revisions that are not
-# installable)
+# (the later is only done for tools with revisions that are not
+# installable).
 
 import argparse
 import subprocess
@@ -30,6 +28,7 @@ from galaxy.tool_util.loader_directory import load_tool_sources_from_path
 
 def clone(toolshed_url, name, owner, repo_path):
     if not os.path.exists(repo_path):
+        print(f"Cloning {toolshed_url} {owner} {name} {repo_path}")
         cmd = [
             "hg",
             "clone",
@@ -42,26 +41,9 @@ def clone(toolshed_url, name, owner, repo_path):
 def get_all_revisions(toolshed_url, name, owner):
     repo_path = f"/tmp/toolshed-{owner}-{name}"
     clone(toolshed_url, name, owner, repo_path)
-    cmd = ["hg", "update", "tip"]
     cmd = ["hg", "log", "--template", "{node|short}\n"]
     result = subprocess.run(cmd, cwd=repo_path, capture_output=True, text=True)
     return list(reversed(result.stdout.splitlines()))
-
-
-def get_all_versions(toolshed_url, name, owner, revisions):
-    repo_path = f"/tmp/toolshed-{owner}-{name}"
-    clone(toolshed_url, name, owner, repo_path)
-
-    versions = {}
-    for r in revisions:
-        cmd = ["hg", "update", r]
-        subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        versions[r] = set()
-        for _, tool in load_tool_sources_from_path(repo_path):
-            versions[r].add((tool.parse_id(), tool.parse_version()))
-
-    return versions
 
 
 def fix_uninstallable(lockfile_name, toolshed_url):
@@ -74,7 +56,7 @@ def fix_uninstallable(lockfile_name, toolshed_url):
     for i, tool in enumerate(tools):
         name = tool["name"]
         owner = tool["owner"]
-
+        print(f"Checking {toolshed_url} {owner} {name} ")
         # get ordered_installable_revisions from oldest to newest
         ordered_installable_revisions = (
             ts.repositories.get_ordered_installable_revisions(name, owner)
@@ -82,7 +64,6 @@ def fix_uninstallable(lockfile_name, toolshed_url):
 
         if len(set(tool["revisions"]) - set(ordered_installable_revisions)):
             all_revisions = get_all_revisions(toolshed_url, name, owner)
-            # all_versions = get_all_versions(toolshed_url, name, owner, all_revisions)
 
         to_remove = []
         to_append = []
@@ -90,7 +71,7 @@ def fix_uninstallable(lockfile_name, toolshed_url):
             if cur in ordered_installable_revisions:
                 continue
             if cur not in all_revisions:
-                print(f"{cur} is not a valid revision of {name} {owner}")
+                print(f"Removing {cur} -- it is not a valid revision of {name} {owner}")
                 to_remove.append(cur)
                 continue
             start = all_revisions.index(cur)
@@ -98,18 +79,22 @@ def fix_uninstallable(lockfile_name, toolshed_url):
             for i in range(start, len(all_revisions)):
                 if all_revisions[i] in ordered_installable_revisions:
                     nxt = all_revisions[i]
+                    break
             if nxt:
-                print(f"remove {cur} in favor of {nxt} {name} {owner}")
+                print(f"Removing {cur} in favor of {nxt} {name} {owner}")
                 to_remove.append(cur)
                 if nxt not in tool["revisions"]:
-                    print(f"adding {nxt} which was absent so far {name} {owner}")
-                    to_append(nxt)
+                    print(f"Adding {nxt} which was absent so far {name} {owner}")
+                    to_append.append(nxt)
             else:
-                print(f"Could not determine next revision for {cur} {name} {owner}")
+                print(f"Could not determine the next revision for {cur} {name} {owner}")
 
         for r in to_remove:
             tool["revisions"].remove(r)
         tool["revisions"].extend(to_append)
+
+        # maintaing unified sorting standard
+        tool["revisions"] = sorted(list(set(map(str, tool['revisions']))))
 
     with open(lockfile_name, "w") as handle:
         yaml.dump(lockfile, handle, default_flow_style=False)
